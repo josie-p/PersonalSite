266abde529f1a850f9347e5df3189f94462d01a9
Your Name <parker.josie.eden@gmail.com>
2023-02-28 12:49:37 -0600

    navbar finished, no broken links, nothing styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div id="navbar">
-      <h2> I am navbar</h2>
+<header>
+      <h1>Josie Parker</h1>
+      <nav>
+        {/* <a href="/index.html">home</a>
+        <a href="/about.html">about me</a>
+        <a href="/portfolio.html">portfolio</a>
+        <a href="mailto:emailme@example.com" title="Contact Josie"
+          >contact me!</a
+        > */}
+        <Link to='/'>home</Link>
+        <Link to='/about'>about me</Link>
+        <Link to='/portfolio'>portfolio</Link>
+        <a href="mailto:parker.josie.eden@gmail.com" title="Contact Josie">contact me!</a>
+      </nav>
+
+    </header>
     </div>
-  );
+  )
 };
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Main } from "./components";
+import { Main, About, Home, Portfolio } from "./components";
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    createRoutesFromElements,
+  } from "react-router-dom";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Main/>}>
+            <Route index element={<Home/>} />
+            <Route path="about" element={<About/>}/>
+            <Route path="portfolio" element={<Portfolio/>} />
+        </Route>
+    )
+)
+
 
 const container = document.getElementById("app");
 const root = ReactDOM.createRoot(container);
 
-root.render(<Main />);
+root.render(<RouterProvider router={router} />);
