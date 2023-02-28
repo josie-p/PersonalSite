f2279f109a0e0affdd84ae58ef3a148585276075
Your Name <parker.josie.eden@gmail.com>
2023-02-28 13:39:56 -0600

    added content to about and portfolio and images and such- need styling

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,7 +3,52 @@ import React from 'react';
 const Portfolio = () => {
     return(
         <div>
-            <h1>hello world from portfolio</h1>
+               <main>
+      <div>
+        <div>
+          {/* <img
+            src="/images/folio-ph1.png"
+            alt="Man at computer"
+          /> */}
+          <h3>Clicker Game</h3>
+          <a href='https://boisterous-travesseiro-636639.netlify.app' target='_blank'>Clicker Game</a>
+          <p>
+            Pork belly beard irony stumptown. Raw denim dreamcatcher gatekeep,
+            authentic chicharrones butcher fashion axe tacos lo-fi hell of
+            taiyaki disrupt freegan tote bag. Mumblecore small batch literally
+            jean shorts ascot, disrupt knausgaard PBR&B selfies. Air plant deep
+            v subway tile taiyaki.
+          </p>
+        </div>
+        <div >
+          {/* <img src="images/folio-ph3.jpg" alt="laptop" /> */}
+          <h3>Tic-Tac-Toe</h3>
+          <a href='https://meek-beijinho-e76832.netlify.app' target='_blank'>Tic-Tac-Toe</a>
+          <p>
+            Pork belly beard irony stumptown. Raw denim dreamcatcher gatekeep,
+            authentic chicharrones butcher fashion axe tacos lo-fi hell of
+            taiyaki disrupt freegan tote bag. Mumblecore small batch literally
+            jean shorts ascot, disrupt knausgaard PBR&B selfies. Air plant deep
+            v subway tile taiyaki.
+          </p>
+        </div>
+        <div>
+          {/* <img
+            src="images/folio-ph2.png"
+            alt="Man at computer"
+          /> */}
+          <h3>Puppy Bowl</h3>
+          <a href='https://elegant-kashata-207cc6.netlify.app' target='_blank'>Puppy Bowl</a>
+          <p>
+            Pork belly beard irony stumptown. Raw denim dreamcatcher gatekeep,
+            authentic chicharrones butcher fashion axe tacos lo-fi hell of
+            taiyaki disrupt freegan tote bag. Mumblecore small batch literally
+            jean shorts ascot, disrupt knausgaard PBR&B selfies. Air plant deep
+            v subway tile taiyaki.
+          </p>
+        </div>
+      </div>
+    </main>
         </div>
     )
 }
